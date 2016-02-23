e525d75650a8112d4e0ba958884a135786ca998c
Selivia <805489580@qq.com>
2016-02-24 00:04:44 +0800

    mytest

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Selivia on 2016-2-23.
+ */
+public class Test
+{
+    public static void show()
+    {
+        System.out.println("a");
+    }
+
+    public static void main(String[] args)
+    {
+        show();
+    }
+}
